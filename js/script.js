8bf07e602aeb7e655ac1d7ad9406e4852b6ab7ba
Subrata Kuila <kuilasubrata01@gmail.com>
2025-08-27 19:59:17 +0530

    i

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function minSec(seconds) {
 async function getSongs(folder) {
 
     currfolder = folder;
-    const base = `/${currfolder}/`;
+    const base = `https://spotify-play.netlify.app/${currfolder}/`;
 
     const res = await fetch(base);
     const html = await res.text();
@@ -99,7 +99,7 @@ async function displayAlbums() {
     }
 
     // ✅ FIX 1: Corrected the IP Address
-    const songsURL = `/songs/`;
+    const songsURL = `https://spotify-play.netlify.app/songs/`;
     console.log("Starting to fetch albums...");  //debug
 
     try {
